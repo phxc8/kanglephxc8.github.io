4fb547462e534fb60efc83a7cb85a3b7779065e4
phxc8 <admin@phxc8.com>
2020-02-14 22:31:10 +0800

    php

---
+++ php/completed/5.5/tpl_php55.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum -y install bzip2-devel libxml2-devel curl-devel db4-devel libjpeg-devel libpng-devel freetype-devel pcre-devel zlib-devel sqlite-devel libmcrypt-devel unzip bzip2
+yum -y install mhash-devel openssl-devel
+yum -y install libtool-ltdl libtool-ltdl-devel
+PREFIX="/vhs/kangle/ext"
+wget -c https://github.phxc8.com/php/completed/5.5/tpl_php55.tar.bz2 -O tpl_php55.tar.bz2
+tar -jxvf tpl_php55.tar.bz2
+mv tpl_php55 $PREFIX
+rm -rf /tmp/*
+/vhs/kangle/bin/kangle -r
